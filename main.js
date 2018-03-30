edd041a237070debdc77622b008ffffa7e9b4cd9
Andy <amillion3@gmail.com>
2018-03-30 15:40:33 -0500

    added basic XHR call functions

--- main.js
+++ main.js
@@ -1,42 +1,17 @@
-//buildPlanetCards
-//calls makeXHURCall and hopefully gets the return
-//from XHR success, store in a variable?
-//if this works, same idea for the build
-
-const buildPlanetCards = () => {
-  let results = makeXHRCall();
-  console.log(results);
-};
-
 function XHRfailure() {
   console.log("Whoopsies!");
 }
 
-function XHRsuccessMini() {
-  const someData = JSON.parse(this.responseText);
-  console.log("xhr success", someData.planets);
-  return someData.planets;
+function XHRsuccess() {
+  const data = JSON.parse(this.responseText);
+  // do something with data.planets;
 }
 
-const makeXHRCall = (actionType) => {
+const startUpApplication = () => {
   const requestData = new XMLHttpRequest();
-  
-  if (actionType === "miniPlanets") {
-    requestData.addEventListener('load', XHRsuccessMini);
-  } else if (actionType === "bigPlanet") {
-    requestData.addEventListener('load', XHRsuccessBig);
-  }
+  requestData.addEventListener('load', XHRsuccess);
   requestData.addEventListener('error', XHRfailure);
   requestData.open("GET", "/db/planets.json");
   requestData.send();
 };
-
-window.onload = function() {
-  buildPlanetCards("miniPlanets");
-  // buildBigPlanetCard("bigPlanet");
-};
-
-// const startUpApplication = () => {
-//   const data = makeXHRCall();
-// };
-// startUpApplication();
+startUpApplication();
