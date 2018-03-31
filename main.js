694811f538bd4e2bf4d84f40dbed4e50b7744e8e
Andy <amillion3@gmail.com>
2018-03-31 11:54:03 -0500

    added full functionality of building the single planet card

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ const showAllPlanetCards = (e) => {  // Show all mini-planet cards
   }
 };
 
-
-
-//Need to verify this works still!!
-const hideBigPlanetCard = () => {  // Hides the big planet card
-  const bigPlanetCardToHide = document.getElementById("planet-full");
-  bigPlanetCardToHide.classList.add("super-hide");
+const showBigPlanetCard = () => {  // Shows the big planet card
+  const bigPlanetCardToShow = document.getElementById("planet-full");
+  bigPlanetCardToHide.classList.remove("super-hide");
 };
 
-
+const hideBigPlanetCard = () => {  // Hides the big planet card
+  const bigPlanetCardToHide = document.getElementById("planet-full");
+  bigPlanetCardToHide.classList.add('hide');
+};
 
 const planetCardsMouseEnter = (e) => {  //Show image, hide text
   e.target.childNodes[3].classList.remove("hide");
@@ -34,13 +34,13 @@ const planetCardsMouseLeave = (e) => {  //Hide image, show text
 
 const planetCardsClick = (e) => {  //Click on a mini-planet card
   hideAllPlanetCards(e);
-  console.log(e);
-  //identify which planet clicked
+  const planet = e.target.parentNode.childNodes[1].innerText;
+  gatherPlanetName(planet);
 };
 
 const bigPlanetCardXClick = (e) => {
-  showAllPlanetCards(e);
-  hideBigPlanetCard(); //need to verify this works
+  sendXHR("default");
+  hideBigPlanetCard();
 }
 
 const createEventListenerMiniCards = () => {
@@ -54,9 +54,10 @@ const createEventListenerMiniCards = () => {
 
 const createEventListenerPlanetCardX = () => {
   const bigPlanetCards = document.getElementsByClassName("close-full-icon");
-  for (let i = 0; i < bigPlanetCards.length; i++) {
-    bigPlanetCards[i].addEventListener('click', bigPlanetCardXClick)
-  }
+  bigPlanetCards[0].addEventListener('click', bigPlanetCardXClick);
+  // for (let i = 0; i < bigPlanetCards.length; i++) {
+  //   bigPlanetCards[i].addEventListener('click', bigPlanetCardXClick);
+  // }
 };
 
 const printToDom = (domString,divId) => {
@@ -76,25 +77,27 @@ const buildPlanetCards = (input) => {  //Mini-planet cards
   createEventListenerMiniCards();
 };
 
-const buildBigPlanetCard = (input) => {  //BIG planet cards
+const gatherPlanetName = (input) => {
+  sendXHR(input);
+};
+
+const buildBigPlanetCard = (input) => {  //BIG planet card
   let domOutput = "";
-  for (let i = 0; i < input.length; i++) {
     domOutput += `
       <div class="close-full-icon">
         <img src="/images/redx.svg.png">
       </div>
       <div class="planet-full-info">
-        <h1>${input[i].name}</h1>
-        <img class="planet-full-main" src="${input[i].imageURL}">
-        <!-- <img class="planet-full-comparison hide" src="${input[i].imageCompURL}">   on hover, show this  -->
+        <h1>${input.name}</h1>
+        <img class="planet-full-main" src="${input.imageURL}">
+        <!-- <img class="planet-full-comparison hide" src="${input.imageCompURL}">   on hover, show this  -->
         <div class="planet-full-description">
-        <p>${input[i].description}</p> <!-- Bottom border -->
-        <p class="center-text">Gas planet? ${input[i].isGasPlanet}</p>
-        <p class="center-text">Number of moons: ${input[i].numberOfMoons}</p>
-        <p class="center-text">Largest moon: ${input[i].nameOfLargestMoon}</p>
+        <p>${input.description}</p> <!-- Bottom border -->
+        <p class="center-text">Gas planet? ${input.isGasPlanet}</p>
+        <p class="center-text">Number of moons: ${input.numberOfMoons}</p>
+        <p class="center-text">Largest moon: ${input.nameOfLargestMoon}</p>
       </div>
     </div>`;
-  }
   printToDom(domOutput,"planet-full");
   createEventListenerPlanetCardX();
 };
@@ -103,17 +106,35 @@ function XHRfailure() {
   console.log("Whoopsies!");
 }
 
-function XHRsuccess() {
+function XHRsuccessMini() {
   const data = JSON.parse(this.responseText);
   buildPlanetCards(data.planets);
-  buildBigPlanetCard(data.planets);
 }
 
-const startUpApplication = () => {
+function XHRsuccessBig() {
+  const data = JSON.parse(this.responseText);
+  buildBigPlanetCard(data.planets);
+
+}
+
+const sendXHR = (input) => {
   const requestData = new XMLHttpRequest();
-  requestData.addEventListener('load', XHRsuccess);
+  if (input === "default") {  // Always builds Mini-planet cards
+    requestData.addEventListener('load', XHRsuccessMini);
+  } else {  //Big planet card
+    //call anonymous function to get JSON and then identify
+    //  only the planet that was clicked and print it
+    requestData.addEventListener('load', function() {
+      const data = JSON.parse(this.responseText);
+      for (let i = 0; i < data.planets.length; i++) {
+        if (data.planets[i].name === input) {
+          buildBigPlanetCard(data.planets[i]);
+        }
+      }
+    });
+  }
   requestData.addEventListener('error', XHRfailure);
   requestData.open("GET", "/db/planets.json");
   requestData.send();
 };
-startUpApplication();
+sendXHR("default");
